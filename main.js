7d605137c319422e0613d4e64ddd71bc2eefb690
PRIYANKA S <109449825+PriyankagithubS@users.noreply.github.com>
2023-02-27 11:57:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let MenuBtn=document.getElementById('MenuBtn')
+ MenuBtn.addEventListener('click',function(e){
+      document.querySelector('body'),this.classList.toggle('mobile-nav-active');
+      this.classList.toggle("MenuBtn")
+ })
+
+
+
+          
+var typed =new Typed(".auto-input",{
+                    strings:["WEB DEVELOPEMENT","IOT"],
+                    typeSpeed:150,
+                    backSpeed:150,
+                    loop:true,
+    
+ });
+            
